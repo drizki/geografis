1819e2bae7ddec29f81bf8e28c7bee044c23e382
Rizki <dwi.rizki.irawan@gmail.com>
2022-10-08 19:48:31 +0700

    fix: rename test function name

--- tests/getGeometry.test.js
+++ tests/getGeometry.test.js
@@ -1,11 +1,11 @@
 const geografis = require('../src/main.js');
 
-test('geometry.getGeometry should return object', () => {
+test('geografis.getGeometry should return object', () => {
     const geometry = geografis.getGeometry('32.73.08.1002', './data/geojson');
     expect(typeof geometry).toBe('object');
 })
 
-test('geometry.getGeometry should return object with complete properties', () => {
+test('geografis.getGeometry should return object with complete properties', () => {
     const geometry = geografis.getGeometry('32.73.08.1002', './data/geojson');
     expect(Object.keys(geometry.properties)).toEqual([
         'code', 
@@ -22,19 +22,19 @@ test('geometry.getGeometry should return object with complete properties', () =>
     ]);
 })
 
-test('geometry.getGeometry should throw error if code is empty', () => {
+test('geografis.getGeometry should throw error if code is empty', () => {
     expect(() => geografis.getGeometry()).toThrow('Parameter code is required');
 })
 
-test('geometry.getGeometry should throw error if geoJsonDir is empty', () => {
+test('geografis.getGeometry should throw error if geoJsonDir is empty', () => {
     expect(() => geografis.getGeometry('32.73.08.1002')).toThrow('Parameter geoJsonDir is required');
 })
 
-test('geometry.getGeometry should throw error if code is not string', () => {
+test('geografis.getGeometry should throw error if code is not string', () => {
     expect(() => geografis.getGeometry(1, './data/geojson')).toThrow('Parameter code must be string');
 })
 
-test('geometry.getGeometry should return empty if geojson could not be found', () => {
+test('geografis.getGeometry should return empty if geojson could not be found', () => {
     const geometry = geografis.getGeometry('32.73.08.1002.1', './data/geojson');
     expect(geometry).toEqual({});
 })
