3d29b4a095f4582021484118a652e12e9e363ed6
Rizki <dwi.rizki.irawan@gmail.com>
2022-10-08 19:49:23 +0700

    fix: rename test function name

--- tests/getNearest.test.js
+++ tests/getNearest.test.js
@@ -3,20 +3,20 @@ const geografis = require('../src/main.js');
 const myLatitude = -6.875218;
 const myLongitude = 107.625931;
 
-test('geometry.getNearest should return correct nearest village name', () => {
+test('geografis.getNearest should return correct nearest village name', () => {
     const nearest = geografis.getNearest(myLatitude, myLongitude);
     expect(nearest.village).toBe('Cigadung'); 
 })
 
-test('geometry.getNearest should return correct nearest village code', () => {
+test('geografis.getNearest should return correct nearest village code', () => {
     const nearest = geografis.getNearest(myLatitude, myLongitude);
     expect(nearest.code).toBe('32.73.18.1004'); 
 })
 
-test('geometry.getNearest should throw error if latitude or longitude is empty', () => {
+test('geografis.getNearest should throw error if latitude or longitude is empty', () => {
     expect(() => geografis.getNearest()).toThrow('Parameter latitude and longitude is required');
 })
 
-test('geometry.getNearest should throw error if latitude or longitude is not number', () => {
+test('geografis.getNearest should throw error if latitude or longitude is not number', () => {
     expect(() => geografis.getNearest('a', 'b')).toThrow('Parameter latitude and longitude must be number');
 })
